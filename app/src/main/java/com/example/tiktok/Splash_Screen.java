5f3e7b9081b0ed2b667383eba6bf33622058a6b6
Arya raj <aryaraj222005@gmail.com>
2023-05-24 22:10:09 +0530

    first Commit

--- app/src/main/java/com/example/tiktok/Splash_Screen.java
+++ app/src/main/java/com/example/tiktok/Splash_Screen.java
@@ -2,7 +2,10 @@ package com.example.tiktok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import kotlinx.coroutines.internal.ThreadSafeHeap;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -10,6 +13,21 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        Thread thread = new Thread(){
+          public void run(){
+            try {
+                sleep(4000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                Intent intent = new Intent(Splash_Screen.this , MainActivity.class) ;
+                startActivity(intent);
+                finish();
+            }
+          }
+        };thread.start();
 
 
 
